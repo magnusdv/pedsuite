3fc16009d48ccc8574344767829e9169510ad2fd
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2022-01-06 17:17:58 +0100

    Tweak package description

--- R/pedsuite-package.R
+++ R/pedsuite-package.R
@@ -1,9 +1,9 @@
 #' pedsuite: A collection of packages for pedigree analysis in R
 #'
-#' The 'ped suite' is a collection of packages for pedigree analysis, covering
-#' applications in forensic genetics, medical genetics and others. A detailed
-#' presentation of the 'ped suite' is given in the book 'Pedigree Analysis in R'
-#' (Vigeland, 2021, ISBN: 9780128244302).
+#' This package makes it easy to install and load the **ped suite** packages for
+#' pedigree analysis in R, covering applications in forensic genetics, medical
+#' genetics and others. A detailed presentation of the **ped suite** is given in
+#' the book *Pedigree Analysis in R* (Vigeland, 2021, ISBN: 9780128244302).
 #'
 #' @docType package
 #'
