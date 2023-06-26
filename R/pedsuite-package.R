41b7003dec783898cd64c061521b083da4d44ec7
Magnus Dehli Vigeland <magnusdv@medisin.uio.no>
2023-06-27 01:49:52 +0200

    Update package documentation

---
+++ R/pedsuite-package.R
@@ -0,0 +1,20 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#'
+#' @import pedtools
+#' @import ribd
+#' @import pedprobr
+#' @import forrel
+#' @import verbalisr
+#'
+#' @importFrom dvir jointDVI
+#' @importFrom ibdsim2 ibdsim
+#' @importFrom paramlink2 lod
+#' @importFrom pedbuildr reconstruct
+#' @importFrom pedmut mutationModel
+#' @importFrom segregatr FLB
+#'
+## usethis namespace: end
+NULL
